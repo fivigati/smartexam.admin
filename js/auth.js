9a74123cbefe1ae7b92b114b552322e068801bec
Fivigati Nurhayati <fivigati@gmail.com>
2026-05-26 04:39:00 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,6 +6,12 @@ async function login() {
   const password =
     document.getElementById('password').value.trim();
 
+  const button =
+    document.getElementById('loginBtn');
+
+  const text =
+    document.getElementById('loginText');
+
   if(!npsn || !password) {
 
     alert('NPSN dan password wajib diisi');
@@ -13,9 +19,48 @@ async function login() {
     return;
   }
 
+  // LOADING STATE
+  button.disabled = true;
+
+  button.classList.add(
+    'opacity-70',
+    'cursor-not-allowed'
+  );
+
+  text.innerHTML = `
+    <div class="flex items-center justify-center gap-2">
+
+      <svg
+        class="animate-spin h-4 w-4 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24">
+
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4">
+        </circle>
+
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8v4l3-3-3-3v4a10 10 0 00-10 10h2z">
+        </path>
+
+      </svg>
+
+      <span>Memverifikasi...</span>
+
+    </div>
+  `;
+
   const result = await apiRequest({
 
-    action: 'login',
+    action:'login',
     npsn,
     password
 
@@ -25,17 +70,33 @@ async function login() {
 
   if(result.success) {
 
-    localStorage.setItem(
-      'smart_exam_user',
-      JSON.stringify(result.user)
-    );
+    text.innerHTML = 'Berhasil Login...';
 
-    window.location.href =
-      'dashboard.html';
+    setTimeout(() => {
+
+      localStorage.setItem(
+        'smart_exam_user',
+        JSON.stringify(result.user)
+      );
+
+      window.location.href =
+        'dashboard.html';
+
+    }, 700);
 
   } else {
 
     alert(result.message);
+
+    button.disabled = false;
+
+    button.classList.remove(
+      'opacity-70',
+      'cursor-not-allowed'
+    );
+
+    text.innerHTML =
+      'Masuk ke Dashboard';
 
   }
 
