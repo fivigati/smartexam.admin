b35dcef52dd5e47bab176e1c4128b9c858c6c6d9
Fivigati Nurhayati <fivigati@gmail.com>
2026-05-25 23:19:30 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,29 @@
+const API_URL =
+'https://script.google.com/macros/s/AKfycbx3SamwedBeCTTJGfN3BXMr0CEePYNupPsbxspwGM4GzA2izbWy5we-qo_SBWASh51BRA/exec';
 
+async function apiRequest(payload) {
+
+  try {
+
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+
+    return await response.json();
+
+  } catch(err) {
+
+    console.error(err);
+
+    return {
+      success: false,
+      message: 'Server error'
+    };
+
+  }
+
+}
